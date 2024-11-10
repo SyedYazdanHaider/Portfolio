327ba2ac3a12611f0f1bff9d4128d51ca7558919
SyedYazdanHaider <yhzaidi7@gmail.com>
2024-11-10 16:30:52 +0500

    Updated00

--- app/About/page.tsx
+++ app/About/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from '../components/navbar'
 import Image from 'next/image'
-import Footer from '../components/footer'
+
 
 const About = () => {
   return (
@@ -13,12 +13,16 @@ const About = () => {
         <div className="max-w-lg text-center md:text-center flex-1">
           <h2 className="text-3xl font-semibold mt-14 text-left pb-8"><u>About Me</u></h2>
           <p className="leading-relaxed text-2xl">
-            I&apos;m Syed Yazdan Haider, the founder of  
-            <br/><i><u>SYHZ Developers</u></i> and an enthusiastic learner of Artificial Intelligence. I&apos;m currently expanding my expertise in AI through the Governor Sindh Initiative on AI and Cloud Computing, gaining hands-on knowledge of the latest technologies and their real-world applications.
+            I&#39;m Syed Yazdan Haider, the founder of  
+            <br/><i><u>SYHZ Developers</u></i> and an enthusiastic learner of Artificial Intelligence. I&#39;m currently expanding my expertise in AI 
+            through the Governor Sindh Initiative on AI and Cloud Computing, gaining hands-on knowledge of the 
+            latest technologies and their real-world applications.
           </p>
           <p className="leading-relaxed mt-4 text-2xl">
-            In parallel with my AI journey, I&apos;m pursuing an A2-level in pre-engineering, 
-            which is enhancing my analytical and problem-solving <br/>skills-qualities that are essential in my development work. At SYHZ Developers, my focus is on creating innovative solutions that leverage technology to address real-world challenges. I’m inspired by the future possibilities and committed to shaping that future through innovation, determination, and continuous learning.
+            In parallel with my AI journey, I&#39;m pursuing an A2-level in pre-engineering, 
+            which is enhancing my analytical and problem-solving <br/>skills-qualities that are essential in my development work. At SYHZ Developers,
+             my focus is on creating innovative solutions that leverage technology to address real-world challenges.
+             I&#39;m inspired by the future possibilities and committed to shaping that future through innovation, determination, and continuous learning.
           </p>
         </div>
         
--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -30,14 +30,16 @@ const Navbar = () => {
 
   return (
     <nav
-      className={` fixed top-0 left-0 w-full bg-gray-600 p-3 text-white p-4 transition-all duration-300 ${
+      className={` fixed top-0 left-0 w-full bg-gray-600  text-white p-4 transition-all duration-300 ${
         showNavbar ? 'transform translate-y-0' : 'transform -translate-y-full '
       }`}
     >
       <div className="container mx-auto flex justify-between items-center">
-        <div className="text-transparent bg-clip-text bg-gradient-to-r from-white  to-pink-500 animate-pulse drop-shadow-[0_0_5px_rgba(236,72,153,0.8)] text-3xl font extrabold">
+        <div className="text-transparent bg-clip-text bg-gradient-to-r from-white  to-pink-500 
+        animate-pulse drop-shadow-[0_0_5px_rgba(236,72,153,0.8)] text-3xl font extrabold">
          <Link href="/">  SYHZ </Link></div>
-        <div className="justify-center text-3xl  text-transparent bg-clip-text bg-gradient-to-r from-blue-700 to-green-800 font-extrabold drop-shadow-[0_0_5px_rgba(255,255,255,0.8)] ">PORTFOLIO</div>
+        <div className="justify-center text-3xl  text-transparent bg-clip-text bg-gradient-to-r from-blue-700 to-green-800 
+        font-extrabold drop-shadow-[0_0_5px_rgba(255,255,255,0.8)] ">PORTFOLIO</div>
         <ul className="flex space-x-4 mr-2">
           <li><Link href="/" className='hover:text-orange-400  '>Home</Link></li>
           <li><Link href="/About" className='hover:text-orange-400 ' >About</Link></li>
